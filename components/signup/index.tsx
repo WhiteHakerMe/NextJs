3a3ed876160a5f3413d5cd3b6576f9acb1711501
haker <140898334+WhiteHakerMe@users.noreply.github.com>
2024-03-28 10:42:53 +0500

    Avion

--- components/signup/index.tsx
+++ components/signup/index.tsx
@@ -34,12 +34,12 @@ const SignUp = () => {
     return (
         <section className='pt-24 pb-[86px] '>
             <div className='container'>
-                <div className='w-full pt-24 h-[400px] bg-slate-500'>
+                <div className='w-full pt-24 h-auto bg-slate-500 overflow-hidden pb-8'>
                     <div className='max-w-[494px] w-full mx-auto'>
-                        <h3 className='text-[32px] text-white font-normal leading-[45px] mb-3'>Join the club and get the benefit</h3>
+                        <h3 className='text-[32px] text-white font-normal leading-[45px] mb-3 '>Join the club and get the benefit</h3>
                         <p className='text-lg font-normal leading-7 text-center text-white'>Sign up for our newsletter and receive exclusive offers on new ranges, sales, pop up stores and more</p>
 
-                        <div className='flex items-center gap-9 pt-[30px] justify-center'>
+                        <div className='flex items-center gap-9 pt-[30px] justify-center '>
                             {EmailData.map((el, i) => (
                                 <div key={i} className='flex flex-row-reverse items-center gap-1 text-base font-normal text-white'>{el.title}
                                 <span><Icons.checkIcon  /></span></div>
@@ -47,7 +47,7 @@ const SignUp = () => {
                         </div>
                     </div>
 
-                    <form className='mt-10 flex items-center justify-center w-full ' onSubmit={(e) => onSubmit(e)}>
+                    <form className='mt-10 flex items-center justify-center w-full' onSubmit={(e) => onSubmit(e)}>
                         <input type="email" placeholder='your@email.com' className='w-1/4 py-4 px-8' onChange={(e) => setEmail(e.target.value)} name="email" />
                         <button type='submit' className='py-4 px-8 1/4 bg-blue-950 text-white'>Sign Up</button>
                     </form>
